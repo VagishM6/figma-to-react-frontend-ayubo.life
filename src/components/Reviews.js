d0952801173b1f284bf1dcf96111e4658fa5d71b
Vagish Mohanakumar <iamvagish6@gmail.com>
2025-03-26 12:34:28 +0530

    modified the review page header layout

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -46,15 +46,16 @@ const Reviews = () => {
             <main className='dash-main'>
                 <div className='dash-main-title'>
                     <div className='ayubo-life'>
-                        <h2 className='dash-main-title'>Reviews</h2>
+                        <h2 className='dash-main-title' style={{marginBottom:"1rem"}}>Review Page</h2>
 
                         {/* <h1 style={{fontSize:"2rem"}}>Review Page</h1> */}
 
-                        <button onClick={showPopup}>popup</button>
-
-                        <p>
+                        <p style={{marginBottom:"1rem"}} >
                             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
                         </p>
+
+                        <button onClick={showPopup}>review window</button>
+
 
 
                         {state && 
